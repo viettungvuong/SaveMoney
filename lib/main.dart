6bbd768057ee641e5570c2f1eeda085eff175094
viettungvuong <viettungvuong@gmail.com>
2023-03-26 21:34:13 +0700

    Import flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() {
   runApp(const MyApp());
 }
