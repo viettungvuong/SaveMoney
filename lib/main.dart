2999bc075775da05b7d4ef7a96d7ab2e03b67322
viettungvuong <viettungvuong@gmail.com>
2023-03-26 21:35:17 +0700

    Thêm cái initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() {
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
