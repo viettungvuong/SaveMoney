2db4199ebbd597fbcf316155e614dcd1a5f22a05
viettungvuong <viettungvuong@gmail.com>
2023-04-13 20:53:37 +0700

    Fix lôỗi build ios

--- lib/main.dart
+++ lib/main.dart
@@ -92,7 +92,6 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: selectedIndex,
