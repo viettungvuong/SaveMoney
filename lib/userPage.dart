b995fefbd77eb97647f241b8d730d5e8fb370f42
viettungvuong <viettungvuong@gmail.com>
2023-05-17 10:48:46 +0700

    Thêm async

--- lib/userPage.dart
+++ lib/userPage.dart
@@ -3,117 +3,104 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-String userName="";
-String password="";
+String userName = "";
+String password = "";
 
-Future<bool> accountExists(String name, String password) async {
+Future<bool> accountExists(String email, String password) async {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
   try {
     List<String> signInMethods = await _auth.fetchSignInMethodsForEmail(email);
 
     if (signInMethods.isNotEmpty) {
-      return true;
-      // ton tai tai khoan
+      return true; // Account exists
     } else {
-      return false;
-      // Account does not exist, you can proceed with signing up or displaying an error message
+      return false; // Account does not exist
     }
   } catch (e) {
-    return false;
-    // Handle error scenario
+    return false; // Handle error scenario
   }
 }
 
-void login(){
+void login() {}
 
-}
+void signup() {}
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late Future<bool> _accountExistsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _accountExistsFuture = Future<bool>.value(false);
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-      ),
-
+      appBar: AppBar(),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-
             TextField(
               decoration: InputDecoration(
                 hintText: 'Email',
               ),
               keyboardType: TextInputType.number,
-              onChanged: (value){
+              onChanged: (value) {
                 setState(() {
-                  if (value.contains('@')) //phai co ki tu email
-                       userName=value as String;
+                  if (value.contains('@')) userName = value as String;
                 });
               },
-            ), //chua sdt
+            ),
 
             TextField(
               decoration: InputDecoration(
                 hintText: 'Mật khẩu',
               ),
               obscureText: true,
-              onChanged: (value){
+              onChanged: (value) {
                 setState(() {
-                  password=value as String;
+                  password = value as String;
                 });
               },
             ),
 
-            ElevatedButton(
-              onPressed: () {
-                // Add your button click logic here
+            FutureBuilder<bool>(
+              future: _accountExistsFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else {
+                  if (snapshot.hasData) {
+                    bool accountExists = snapshot.data!;
+                    return ElevatedButton(
+                      onPressed: () {
+                        // Perform login or signup based on account existence
+                        if (accountExists) {
+                          login();
+                        } else {
+                          signup();
+                        }
+                      },
+                      child: Text(accountExists ? 'Đăng nhập' : 'Đăng ký'),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return Container(); // Handle other cases as desired
+                  }
+                }
               },
-              child: Text('Đăng nhập / Đăng ký'),
             ),
-
           ],
         ),
       ),
