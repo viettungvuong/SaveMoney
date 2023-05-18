07688b18080e2ad31d3cc452e0359dd19e6d3326
viettungvuong <viettungvuong@gmail.com>
2023-05-18 16:11:34 +0700

    Update spending.dart

--- lib/spending.dart
+++ lib/spending.dart
@@ -13,4 +13,12 @@ class Spending{
   }
 }
 
+void swap(List<Spending> spendings){
+
+}
+
+void sortSpending(List<Spending> spendings){
+  spendings.sort((Spending a, Spending b) => (a.spentMoney=0 as double).compareTo(b.spentMoney=0));
+  //nma de coi lai quick sort
+}
 
