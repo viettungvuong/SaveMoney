9961be3d59ae32559a475db8063caf89e02100f6
viettungvuong <viettungvuong@gmail.com>
2023-05-29 10:00:37 +0700

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -322,21 +322,22 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
 
+            SizedBox(
+              height: 100,
+            ),
 
-            /*ListView.separated(    //Element-5
+
+            ListView.separated(
               shrinkWrap: true,  //this property is must when you put List/Grid View inside SingleChildScrollView
               physics: NeverScrollableScrollPhysics(), //this property is must when you put List/Grid View inside SingleChildScrollView
               itemCount: spendings.length,
-              /*itemBuilder: (context, i) {
-                return ListTile(
-                  title: Text(spendings[i].Title),
-                  subtitle: Text(listItems[i].Description),
-                );
-              },*/
+              itemBuilder: (context, i) {
+                return SpendingItem(spending: spendings[i]);
+              },
               separatorBuilder: (context, i) {
                 return Divider();
               },
-            ),*/
+            ),
 
           ],
         ),
